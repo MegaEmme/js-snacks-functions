f7ad1bee651d2013f8ad5aaf98ce5a2f1d3768a2
MegaEmme <mikmusco@gmail.com>
2025-03-03 15:19:16 +0100

    creating for cycle

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -5,9 +5,22 @@ const names = ["Anna", "Luca", "Marco", "Adele", "Giovanni", "Alessandra"];
 
 // Dichiara la funzione qui.
 
+function initials(namesArray){
+
+    let letterArray = [];
+    
+   
+    for(let i = 0; i <= namesArray.length -1; i++){
+        let firstLetter = namesArray.indexOf(i);
+        letterArray[i] += firstLetter;      
+    }
+
+    return letterArray;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
+console.log(initials(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
