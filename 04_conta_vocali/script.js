803c85f36cb53454b4e44b7dff95ea5a3f52edd2
MegaEmme <mikmusco@gmail.com>
2025-03-03 16:35:52 +0100

    started working on es.4

--- 04_conta_vocali/script.js
+++ 04_conta_vocali/script.js
@@ -5,9 +5,19 @@ const word = 'javascript';
 
 // Dichiara la funzione qui.
 
+function findVowels(input){
+
+    let vowels=0;
+    for (let i=0; i<=input.length-1; i++){
+        if (i==='a'|| i==='e' || i==='i'|| i==='o' ||i==='u'){
+            vowels++;
+        }
+    }
+    return vowels;
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(findVowels(word));
 
 //Risultato atteso se si passa 'javascript': 3 (a, a, i)
