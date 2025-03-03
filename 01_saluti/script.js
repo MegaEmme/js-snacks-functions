b3bc0904ce539c8e78ef5c55b5966f1eda2e2f1f
MegaEmme <mikmusco@gmail.com>
2025-03-03 13:52:26 +0100

    solved es.1 + bonus es.1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -2,12 +2,24 @@
 
 const userName = 'Mario';
 
-
 // Dichiara la funzione qui.
 
+function sayHello (input) {
+    let hello = "Ciao " + input;
+    console.log(hello);
+}
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+sayHello(userName);
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
+
+// Arrow function 
+
+// const sayHello=(input) => {
+//     hello = "Ciao " + input;
+//     console.log(hello);
+// }
+
+// sayHello(userName);
