fb3fe692d6251731205a7a2b0818a0a8d5cd36a5
MegaEmme <mikmusco@gmail.com>
2025-03-03 15:39:10 +0100

    add: arrow function (bonus)

--- 02_iniziali/script.js
+++ 02_iniziali/script.js
@@ -27,3 +27,22 @@ console.log(initials(names));
 
 
 //Risultato atteso: ["A", "L", "M", "A", "G", "A"]
+
+//arrow function
+
+// const initials = (namesArray) => {
+//     let letterArray = [];
+    
+//     for(let i = 0; i <= namesArray.length -1; i++){
+
+//         const currentName = namesArray[i];
+
+//         let firstLetter = currentName.at(0);
+
+//         letterArray.push(firstLetter);      
+//     }
+
+//     return letterArray;
+// }
+
+// console.log(initials(names));
