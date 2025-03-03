95590d9877bf786c905a766e14bd3306f2de4ee1
MegaEmme <mikmusco@gmail.com>
2025-03-03 18:51:41 +0100

    solved bonus exercise

--- 05_bonus/script.js
+++ 05_bonus/script.js
@@ -5,14 +5,59 @@ buon pomeriggio se è pomeriggio (fino alle 17)
 e buonasera se è sera (oltre le 17)
 */
 
-const name = 'Mario';
+const aName = 'Mario';
 
 
 // Dichiara la funzione qui.
 
+function differentGreetingTimes (input){
+    
+    let actualTime = new Date().getHours();
+
+    console.log(`Sone le ${actualTime}`);
+
+    let greetings;
+
+    if (actualTime>=6 && actualTime<13){
+        greetings = `Buongiorno ${input}`;
+    } else if (actualTime>=13 && actualTime<17){
+        greetings = `Buon pomeriggio ${input}`;
+    } else if (actualTime>=17 && actualTime <0){
+        greetings = `Buonasera ${input}`;
+    } else {
+        greetings = `Buonanotte ${input}`;
+    }
+    return greetings;
+
+}
+
+
 
 // Invoca la funzione qui e stampa il risultato in console
 
-
+console.log(differentGreetingTimes(aName));
 
 //Risultato atteso se si passa 'Mario' alle 18: // Buonasera Mario.
+
+//arrow function
+
+// const differentGreetingTimes = (input) =>{
+//     let actualTime = new Date().getHours();
+
+//     console.log(`Sone le ${actualTime}`);
+
+//     let greetings;
+
+//     if (actualTime>=6 && actualTime<13){
+//         greetings = `Buongiorno ${input}`;
+//     } else if (actualTime>=13 && actualTime<17){
+//         greetings = `Buon pomeriggio ${input}`;
+//     } else if (actualTime>=17 && actualTime <0){
+//         greetings = `Buonasera ${input}`;
+//     } else {
+//         greetings = `Buonanotte ${input}`;
+//     }
+//     return greetings;
+// }
+
+// console.log(differentGreetingTimes(aName));
