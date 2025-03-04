42d34488744a1a770d56c4f24c3d2c290a9f78c0
MegaEmme <mikmusco@gmail.com>
2025-03-04 09:53:10 +0100

    adjusted es.1

--- 01_saluti/script.js
+++ 01_saluti/script.js
@@ -5,21 +5,18 @@ const userName = 'Mario';
 // Dichiara la funzione qui.
 
 function sayHello (input) {
-    let hello = "Ciao " + input;
-    console.log(hello);
+    return `Ciao ${input}`;
 }
-
 // Invoca la funzione qui e stampa il risultato in console
 
-sayHello(userName);
+console.log(sayHello(userName));
 
 //Risultato atteso se si passa 'Mario': // ciao Mario
 
 // Arrow function 
 
 // const sayHello=(input) => {
-//     hello = "Ciao " + input;
-//     console.log(hello);
+//     return `Ciao ${input}`;
 // }
 
-// sayHello(userName);
+// console.log(sayHello(userName));
